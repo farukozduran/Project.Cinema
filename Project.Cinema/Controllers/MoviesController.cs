67a5188a66d53cf0aecb49ae41eb9f42af7ce7f9
Faruk ÖZDURAN <farukfurkan.ozduran@outlook.com>
2026-06-18 15:52:43 +0300

    Add project files.

---
+++ Project.Cinema/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Cinema.Data;
+using Project.Cinema.Models;
+
+namespace Project.Cinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult MovieList()
+        {
+            var movies = _context.Movies.ToList();
+            return new JsonResult(movies);
+        }
+
+        [HttpPost]
+        public JsonResult AddMovie(Movie movie)
+        {
+            Movie newMovie = new Movie()
+            {
+                MovieTitle = movie.MovieTitle,
+                Genre = movie.Genre,
+                Director = movie.Director,
+                Duration = movie.Duration,
+                Description = movie.Description,
+                ImageUrl = movie.ImageUrl
+            };
+
+            _context.Movies.Add(newMovie);
+            _context.SaveChanges();
+
+            return new JsonResult("Film basariyla eklendi.");
+        }
+
+        [HttpPost]
+        public JsonResult DeleteMovie(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if(movie == null)
+            {
+                return new JsonResult("Film bulunamadi.");
+            }
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return new JsonResult("Film basariyla silindi.");
+        }
+    }
+}
---
+++ Project.Cinema/wwwroot/js/custom.js
@@ -0,0 +1,112 @@
+﻿$(document).ready(function () {
+    if ($('#movieTableBody').length) {
+        loadMovies();
+    }
+});
+
+
+$(document).ready(function () {
+    loadMovies();
+});
+
+
+$(document).ready(function () {
+    if ($('#movieTableBody').length) {
+        loadMovies();
+    }
+});
+
+function loadMovies() {
+    $.ajax({
+        url: '/Movies/MovieList',
+        type: 'GET',
+        success: function (data) {
+            let rows = '';
+
+            $.each(data, function (index, movie) {
+                rows += `
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center gap-3">
+                                <img src="${movie.imageUrl}" 
+                                     style="width:60px; height:80px; object-fit:cover; border-radius:8px;" />
+                                <strong>${movie.movieTitle}</strong>
+                            </div>
+                        </td>
+                        <td>${movie.genre}</td>
+                        <td>${movie.director}</td>
+                        <td>${movie.duration} dk</td>
+                        <td>${movie.description}</td>
+                        <td class="text-end">
+                            <button class="btn btn-sm btn-warning">Düzenle</button>
+                            <button class="btn btn-sm btn-danger" onclick="deleteMovie(${movie.movieId})">Sil</button>
+                        </td>
+                    </tr>
+                `;
+            });
+
+            $('#movieTableBody').html(rows);
+        },
+        error: function () {
+            alert('Filmler listelenirken hata oluştu.');
+        }
+    });
+}
+
+function addMovie() {
+    var movie = {
+        MovieTitle: $('#movieTitle').val(),
+        Genre: $('#genre').val(),
+        Director: $('#director').val(),
+        Duration: $('#duration').val(),
+        Description: $('#description').val(),
+        ImageUrl: $('#imageUrl').val()
+    };
+
+    $.ajax({
+        url: '/Movies/AddMovie',
+        type: 'POST',
+        data: movie,
+        success: function (response) {
+            alert(response);
+
+            clearMovieForm();
+
+            $('#movieModal').modal('hide');
+
+            loadMovies();
+        },
+        error: function () {
+            alert('Film eklenirken hata oluştu.');
+        }
+    });
+}
+function deleteMovie(id) {
+    var confirmDelete = confirm("Bu filmi silmek istediğinize emin misiniz?");
+
+    if (!confirmDelete) {
+        return;
+    }
+
+    $.ajax({
+        url: '/Movies/DeleteMovie',
+        type: 'POST',
+        data: { id: id },
+        success: function (response) {
+            alert(response);
+            loadMovies();
+        },
+        error: function () {
+            alert('Film silinirken hata oluştu.');
+        }
+    });
+}
+
+function clearMovieForm() {
+    $('#movieTitle').val('');
+    $('#genre').val('');
+    $('#director').val('');
+    $('#duration').val('');
+    $('#description').val('');
+    $('#imageUrl').val('');
+}
